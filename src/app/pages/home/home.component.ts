51ae7ab23da0a1773337143eab292db957739a06
Julian Balaguera <julianchoripanxd@gmail.com>
2024-10-17 01:10:25 -0500

    Created slider component and improved code

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { PromoSliderComponent } from '../../components/promo-slider/promo-slider.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [PromoSliderComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
