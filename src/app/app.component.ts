59bdb848f57ccc045211d6db0c1b5194f43946e7
daniel_colina <danielc123avila@icloud.com>
2024-10-16 20:48:07 -0500

    home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,UserProfileComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, UserProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { UserProfileComponent } from './pages/user-profile/user-profile.componen
 import { UpdateUserAddressComponent } from './components/update-user-address/update-user-address.component';
 import { UpdateUserComponent } from './components/update-user/update-user.component';
 import { UserPurchaseHistoryComponent } from './components/user-purchase-history/user-purchase-history.component';
-
+import { importProvidersFrom } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
-     {path:'',component:UserProfileComponent},
+     {path:'',component:HomeComponent},
     {path:'updateUserAddress',component: UpdateUserAddressComponent},
     {path:'updateUser',component:UpdateUserComponent},
-    {path:'userPurchaseHistory',component:UserPurchaseHistoryComponent}
-];
+    {path:'userPurchaseHistory',component:UserPurchaseHistoryComponent},
+    {path: 'user', component:UserProfileComponent }
+    ];
