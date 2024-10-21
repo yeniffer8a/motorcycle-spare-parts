b75fa009170dc3e55f6369b455c97d1318900683
daniel_colina <danielc123avila@icloud.com>
2024-10-21 11:52:29 -0500

    parte dinamica del header y menu hamburguesa lista

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,10 +1,10 @@
 import { Component, inject, OnInit, Input, signal, input } from '@angular/core';
-import { Router, RouterLinkWithHref } from '@angular/router';
+import { Router, RouterLinkWithHref, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cards',
   standalone: true,
-  imports: [RouterLinkWithHref],
+  imports: [RouterLinkWithHref, RouterLink],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css',
 })
@@ -13,8 +13,8 @@ export class CardsComponent  {
   @Input() price = 0 
   @Input() image = ''
 
-  // constructor (private router: Router){}
-  // goToDetail(pageName: string): void{
-  //   this.router.navigate(['${/user}'])
-  // }
+  constructor (private router: Router){}
+  goToDetail(pageName: string): void{
+    this.router.navigate(['${/user}'])
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  activarMenu = false;
 
+  toggleMenu() {
+    this.activarMenu = !this.activarMenu
+  }
+  iraSection(section_id: string) {
+    // this.activarMenu = false;
+    const section = document.getElementById(section_id);
+    this.activarMenu = !this.activarMenu
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
